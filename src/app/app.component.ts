e636fad06869e4bc7b24e0f6e6b232cde91a6c30
Victor Martins <vmartins@fairfax.com.br>
2025-03-25 17:14:40 -0300

    Created the first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { ContainerComponent } from "./components/container/container.component";
+import { ButtonComponent } from "./components/button/button.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, ContainerComponent, ButtonComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
---
+++ src/app/components/container/container.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from "../button/button.component";
+
+@Component({
+  selector: 'app-container',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './container.component.html',
+  styleUrl: './container.component.css'
+})
+export class ContainerComponent {
+
+}
